056a6de74323bb081bec4df9d5d1566dc9c40f71
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-03-31 21:44:04 +0200

    lamps

--- lampy.py
+++ lampy.py
@@ -1,4 +1,4 @@
-vstup = str('OOOxOxOOOOOO');
+vstup = str('OOOXOXOOOOOO');
 vstup =  [vstup[i:i+3] for i in range(0, len(vstup), 3)]
 
 # Základní hodnota pro vstup 
@@ -8,12 +8,13 @@ finish = []
 
 for lamp_t in vstup:
     vastup_3 = list(lamp_t)
-    if 'x' in vastup_3:
-        print(vastup_3)
+    if 'X' in vastup_3:
+
         finish.append(vastup_3)
     else:
-            vastup_3[1] = '-'
             print(vastup_3)
+            vastup_3[1] = '-'
+            print('Y > ', vastup_3)
             finish.append(vastup_3)
 
 print("FINISH")
