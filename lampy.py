43568c322e44e57b6fad849aaf4d303db6153919
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-04-24 22:41:25 +0200

    lamps fix

--- lampy.py
+++ lampy.py
@@ -10,7 +10,7 @@ for lamp_t in vstup:
     vastup_3 = list(lamp_t)
     if 'X' in vastup_3:
         print('X > ', vastup_3)
-        finish.append(vastup_3)
+        finish.append(vastup_3) 
     else:
             print(vastup_3)
             vastup_3[1] = '-'
@@ -19,6 +19,21 @@ for lamp_t in vstup:
 
 print("FINISH")
 finish = str(finish).replace('[', '').replace(']', '').replace(',', '').replace("'", '').replace(' ', '')
+finish =  [finish[i:i+2] for i in range(0, len(finish), 2)]
+string = ''.join(finish)
+print(string)
+print(finish)
+for lamp_t in finish:
+
+    print(vastup_3)
+    if '[O, O]' in vastup_3:
+        vastup_3[1] = ''
+        print('X > ', vastup_3)
+        finish.append('O')
+
+finish = str(finish).replace('[', '').replace(']', '').replace(',', '').replace("'", '').replace(' ', '')
+
+
 print(finish)
 
     
